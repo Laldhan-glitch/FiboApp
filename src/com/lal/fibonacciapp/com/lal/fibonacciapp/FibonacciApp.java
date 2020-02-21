d0bf50ff48d87a35a710d66983fb4bafa94b2f25
unknown <laldhantamang@gmail.com>
2020-02-20 23:19:21 -0500

    GenerateFibonacciSeries class added

--- src/com/lal/fibonacciapp/com/lal/fibonacciapp/FibonacciApp.java
+++ src/com/lal/fibonacciapp/com/lal/fibonacciapp/FibonacciApp.java
@@ -1,4 +1,5 @@
 package com.lal.fibonacciapp;
+
 public class FibonacciApp {
     public static void main(String[] args) {
         System.out.println("hello");
--- src/com/lal/fibonacciapp/com/lal/fibonacciapp/GenerateFibonacciSeries.java
+++ src/com/lal/fibonacciapp/com/lal/fibonacciapp/GenerateFibonacciSeries.java
@@ -1,4 +1,10 @@
 package com.lal.fibonacciapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateFibonacciSeries {
+    public List<Integer> generate(){
+        return new ArrayList<>();
+    }
 }
